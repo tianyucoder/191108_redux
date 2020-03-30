fc427bc9dd7f3a62d8b284566f1719d14093be95
tianyu5177 <zhangtianyu5177@sina.com>
2020-03-30 12:02:47 +0800

    更新

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -9,7 +9,7 @@ import personReducer from './person_reducer'
 //引入combineReducers专门用于合并所有的reducer，最终生成一个总的reducer
 import {combineReducers} from 'redux'
 
-//调用combineReducers所传入的那个对象，就是redux帮我们所保存的总状态
+//调用combineReducers所传入的那个对象，就是redux帮我们所保存的总状态！！！！！！！
 export default combineReducers({
 	number:countReducer,
 	persons:personReducer
