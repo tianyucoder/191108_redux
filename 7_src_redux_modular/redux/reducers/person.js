9b113854969c5211174c09b42deaf8a6de34b865
tianyu5177 <zhangtianyu5177@sina.com>
2020-03-30 19:10:22 +0800

    更新

--- 7_src_redux_modular/redux/reducers/person.js
+++ 7_src_redux_modular/redux/reducers/person.js
@@ -6,7 +6,6 @@ import {ADD_PERSON} from '../action_types'
 export default function (preState=[{name:'沛华',age:18}],action){ //preState=[]的含义是设置初始化状态
 	//从action中获取type、data
 	const {type,data} = action
-	console.log('@@@@@@@@@@@person');
 	//提前准备好一个newState用于返回
 	let newState
 	switch (type) {
